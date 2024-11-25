c2f36448d1153aefba3e8ffbeb4a5064bd1e7415
sherm <shermawns@gmail.com>
2024-11-25 14:55:50 -0300

    Implementing Trilogger services in the controller

---
+++ src/main/java/com/shermawn/BuildingMaintenance/dto/triloggers/RequestTriloggerDTO.java
@@ -0,0 +1,19 @@
+package com.shermawn.BuildingMaintenance.dto.triloggers;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class RequestTriloggerDTO {
+
+    @NotBlank
+    @Email(regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}")
+    private String username;
+
+    @NotBlank
+    private String password;
+}
